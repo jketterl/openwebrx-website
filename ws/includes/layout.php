48a73e953b5c21a2119ba733a0764a55486ec05d
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-06 00:23:44 +0100

    include responsive layout "burger" button

--- ws/includes/layout.php
+++ ws/includes/layout.php
@@ -47,13 +47,16 @@ function layout(string $menu_id, callable $content_generator) {
     <script src="/jquery/jquery.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="navbar-brand">
             <a href="https://www.openwebrx.de">
                 <img src="/gfx/openwebrx-logo-big.png" alt="OpenWebRX" />
             </a>
         </div>
-        <div class="collapse navbar-collapse">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="navbar-nav">
                 <?php foreach($menu as $item) {
                     if (array_key_exists("dropdown", $item)) { ?>
