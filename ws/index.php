359508be6268923914d86710a865499e8f084e01
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-06 23:36:41 +0100

    image fadeout

--- ws/index.php
+++ ws/index.php
@@ -4,6 +4,9 @@ require_once(__DIR__ . "/includes/layout.php");
 layout("home", function(){ ?>
     <div class="container">
         <div class="col-12 openwebrx-background">
+            <div class="image-fadeout">
+                <img src="/gfx/openwebrx-screenshot.png" />
+            </div>
             <h1>OpenWebRX web-based SDR receiver</h1>
         </div>
         <div class="col-12 mt-5">
