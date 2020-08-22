2d925ed33118757de44ce96efbe38d34e5a269f6
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-08-22 15:30:12 +0200

    add footer links in new tab

--- ws/includes/layout.php
+++ ws/includes/layout.php
@@ -118,8 +118,8 @@ function layout(string $menu_id, string $title, callable $content_generator) {
     </nav>
     <?php echo $content_generator(); ?>
     <footer class="footer">
-        <span>Built with <a href="https://getbootstrap.com/">Bootstrap</a></span> |
-        <span><a href="https://bootswatch.com/darkly/">Darkly theme by Thomas Park</a></span> |
+        <span>Built with <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a></span> |
+        <span><a href="https://bootswatch.com/darkly/" target="_blank">Darkly theme by Thomas Park</a></span> |
         <span><a class="footer-item" href="/impressum.php">Impressum</a></span> |
         <span class="twitter-button"><a href="https://twitter.com/dd5jfk?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @dd5jfk</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></span>
     </footer>
